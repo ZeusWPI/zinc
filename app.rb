bd44765b645e4b76199d65b70743d1cf969b19e1
Lorin Werthen <werthenlorin@gmail.com>
2016-09-04 01:10:36 +0200

    init

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'base64'
+
+Tilt.register Tilt::ERBTemplate, 'svg'
+
+get '/:file' do
+  headers 'Content-Type' => 'image/svg+xml'
+
+  c = params['color']
+  css = "path, rect { fill: #{c} !important; stroke: #{c} !important }"
+  encoded_css = Base64.encode64(css)
+  style = %(<?xml-stylesheet type="text/css" href="data:text/css;charset=utf-8;base64,#{encoded_css}" ?>)
+
+  File.read("images/#{params['file']}") + style
+end
