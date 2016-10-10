db577f50b58535ef65388197fe99596584490408
Lorin Werthen <werthenlorin@gmail.com>
2016-10-10 18:48:23 +0200

    make software less gay

--- app.rb
+++ app.rb
@@ -3,12 +3,7 @@ require 'base64'
 
 get '/:file' do
   headers 'Content-Type' => 'image/svg+xml'
-
-  c = params['color']
-  css = "path, rect { fill: #{c} !important; stroke: #{c} !important }"
-  encoded_css = Base64.encode64(css)
-  style = %(<?xml-stylesheet type="text/css" href="data:text/css;charset=utf-8;base64,#{encoded_css}" ?>)
-
-  f = File.read("images/#{params['file']}").lines
-  f[0] + style + f[1..-1].join
+  c = 'black'
+  c = params['color'] if params['color']
+  File.read("images/#{params['file']}").gsub('$COLOR', c)
 end
