fbf7602abaa2d0ad9c971f5085c27b320b586200
lorin <werthenlorin@gmail.com>
2018-07-22 13:08:56 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
 require 'base64'
 
-get '/:file' do
+get '/:color' do
   headers 'Content-Type' => 'image/svg+xml'
   c = 'black'
   c = params['color'] if params['color']
-  File.read("images/#{params['file']}").gsub('$COLOR', c)
+  File.read("images/zeuslogo.svg").gsub('$COLOR', c)
+end
+
+get '/' do
+  headers 'Content-Type' => 'image/svg+xml'
+  File.read("images/zeuslogo.svg").gsub('$COLOR', 'black')
 end
