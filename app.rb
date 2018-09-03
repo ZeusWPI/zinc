119f28e306ec062c18b3817302371774b90e80d6
redfast00 <redfast00@gmail.com>
2018-09-04 00:03:31 +0200

    Fix vulnerability

--- app.rb
+++ app.rb
@@ -4,11 +4,12 @@ require 'base64'
 get '/:color' do
   headers 'Content-Type' => 'image/svg+xml'
   c = 'black'
-  c = params['color'] if params['color']
+  c = params['color'] if params['color'] and !params['color'].match(/\A[a-zA-Z0-9]*\z/).nil?
   File.read("images/zeuslogo.svg").gsub('$COLOR', c)
 end
 
-get '/' do
+get '/?' do
   headers 'Content-Type' => 'image/svg+xml'
   File.read("images/zeuslogo.svg").gsub('$COLOR', 'black')
 end
+
