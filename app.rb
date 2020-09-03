7d8bde5e842b9a7bacff15453fb4b0041fc1b876
Lorin Werthen <werthenlorin@gmail.com>
2020-09-03 09:12:42 +0200

    updates and speedups

--- app.rb
+++ app.rb
@@ -2,17 +2,21 @@
 
 require 'sinatra'
 
+configure do
+  @@LOGO_PRE, @@LOGO_POST = File.read('images/zeuslogo.svg').split('$COLOR')
+end
+
 get '/:color?' do
   headers 'Content-Type' => 'image/svg+xml'
   color = if params[:color] == 'zeus'
             '#ff7f00'
-          elsif /^([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/ =~ params[:color]
+          elsif /^([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/.match?(params[:color])
             '#' + params[:color]
-          elsif /^[a-zA-Z]+$/ =~ params[:color]
+          elsif /^[a-zA-Z]+$/.match?(params[:color])
             params[:color]
           else
             '#000000'
           end
-  File.read('images/zeuslogo.svg').gsub('$COLOR', color)
-end
 
+  String.new.concat(@@LOGO_PRE, color, @@LOGO_POST)
+end
